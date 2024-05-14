1c71cf985e60ea630d047c84bb9efa00d00d79c8
Vincent Thai <vincentthai@live.ca>
2024-05-14 16:43:47 -0400

    added functions to calculate totals, and hit

--- bj_game.py
+++ bj_game.py
@@ -1,7 +1,7 @@
 import random
 
-suits = ["diamond","club","heart","spade"]
-ranks = ["2","3","4","5","6","7","8","9","10","J","Q","K","A" ]
+SUITS = ["diamond","club","heart","spade"]
+RANKS = ["2","3","4","5","6","7","8","9","10","J","Q","K","A" ]
 
 # A class with 2 fields of type String. One stores suit and the other stores rank.
 class Card:
@@ -9,34 +9,60 @@ class Card:
        self.suit = suit
        self.rank = rank
 
-# A Hand is a set of 2 cards, with 2 fields indicating its hard and soft total. 
+# A Hand is a list of Cards, with 2 fields indicating its hard and soft total. 
 # A Hand only has a different soft and hard total when there is an ace.
 # For the dealer, card1 is upcard while card2 is facedown.
+# Hand(Listof Cards, int, int)
 class Hand:
-    def __init__(self,card1,card2,tot,alt_tot) -> None:
-        self.card1 = card1
-        self.card2 = card2
-        if card1.rank == "A" or card2.rank == "A":
-            tot = get_value(card1) + 1
-            alt_tot = get_value(card1) + 11
-        else:
-            tot, alt_tot = get_value(card1) + get_value(card2)
+    def __init__(self,cards,tot,alt_tot):
+        self.cards = cards
+        self.tot = tot
+        self.alt_tot = alt_tot
 
  # get_value is a function which returns the value of a given card. This is the numeric value for 2-9 and 10 for J,Q,K, and 1 or 11 for A           
 def get_value(card):
-    if card.rank in ranks[:8]:
-        return card.rank
-    elif card.rank in ranks[9:11]:
-        return "10"
+    if card.rank in RANKS[:9]:
+        return int(card.rank)
+    elif card.rank in RANKS[9:12]:
+        return int(10)
     else:
         return "A"
 
+# a function that updates the tot and alt tot values of a Hand class following the rules of blackjack
+def get_total(hand):
+    for card in hand.cards:
+        if card.rank == "A":
+            hand.tot += 1
+            hand.alt_tot += 11
+        else:
+            hand.tot += get_value(card)
+            hand.alt_tot = hand.tot
+
+def hit(hand):
+    hand.append(game_deck.pop(0))
+
+# For debuging hands
+def print_hand(hand):
+    for card in hand.cards:
+        print(card.rank,"of",card.suit)
+    print("Tot:",hand.tot,"\n Alt Tot: ",hand.alt_tot)
+
 # Orderd new deck initalized
 new_deck = []
-for rank in ranks:
-    for suit in suits:
+for rank in RANKS:
+    for suit in SUITS:
         card = Card(suit,rank)
         new_deck.append(card)
+
+# Intialize a game deck and shuffle it
+game_deck = [card for card in new_deck]
+random.shuffle(game_deck)
+
+# Deal 2 card to player
+playerhand = Hand([game_deck.pop(0), game_deck.pop(0)],0,0)
+get_total(playerhand)
+
+print_hand(playerhand)
 
 
     
