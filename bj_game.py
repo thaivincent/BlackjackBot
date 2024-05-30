d68049b7c7ee3af57f9b5088bc07d83d6f5dfda9
Vincent Thai <vincentthai@live.ca>
2024-05-29 22:23:12 -0400

    changed function

--- bj_game.py
+++ bj_game.py
@@ -40,8 +40,8 @@ def get_total(hand):
             hand.alt_tot = hand.tot
     return
 
-def hit(hand):
-    hand.append(game_deck.pop(0))
+def hit(hand,deck):
+    hand.append(deck.pop(0))
     return
 
 # For debuging hands
